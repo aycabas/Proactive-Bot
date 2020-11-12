45744ad6c26977ea62d631c8af7b66a8b0bbc2bb
Bob German <bgerman1@hotmail.com>
2020-11-12 18:12:32 -0500

    Now displays response data or error message

--- Proactive Bot/services/customers.js
+++ Proactive Bot/services/customers.js
@@ -2,41 +2,46 @@ class CustomerService {
 
     async getLatestCustomer() {
 
-        let result = 
-        {
-            "id": 1,
-            "firstName": "XERRORTed",
-            "lastName": "James",
-            "profileImage": "https://pbs.twimg.com/profile_images/3647943215/d7f12830b3c17a5a9e4afcc370e3a37e_400x400.jpeg",
-            "gender": "male",
-            "address": "1234 Anywhere St.",
-            "city": " Phoenix ",
-            "state": {
-                "abbreviation": "AZ",
-                "name": "Arizona"
-            },
-            "orders": [
-                { "productName": "Basketball", "itemCost": 7.99 },
-                { "productName": "Shoes", "itemCost": 199.99 }
-            ],
-            "latitude": 33.299,
-            "longitude": -111.963
-        };
+        let result;
 
-        const url = `${process.env.CrmUrl}:${process.env.CrmPort}/api/customers/1`;
-        const response = await fetch(url, {
-            method: "GET",
-            headers: { Accept: "application/json" }
-        })
+        try {
+            const url = `${process.env.CrmUrl}:${process.env.CrmPort}/api/customers/1`;
+            const response = await fetch(url, {
+                method: "GET",
+                headers: { Accept: "application/json" }
+            })
+    
+            console.log (`Received customers with status code ${response.status}`);
+    
+            if (response.status !== 200) {
+                throw `Response error ${response.status}: ${response.statusText}`;
+            }
+            if (response.size <= 0) {
+                throw `Empty response`;
+            }
 
-        console.log (`Received customers with status code ${response.status}`);
-
-        if (response.status === 200) {
-            const json = await response.json();
-            result = json;
+            result = await response.json();
+    
+        }
+        catch (error) {
+            result = 
+            {
+                "id": 1,
+                "firstName": `Error fetching customer data`,
+                "lastName": "",
+                "gender": "",
+                "address": error,
+                "city": "  ",
+                "state": {
+                    "abbreviation": "",
+                    "name": ""
+                },
+                "orders": [],
+                "latitude": 0,
+                "longitude": 0
+            };
         }
         
-
         return result;
     }
 }
