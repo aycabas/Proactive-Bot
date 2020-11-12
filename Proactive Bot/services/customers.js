5a9fa8e3497a36affd59cc03d61f4226d5ca03fd
Bob German <bgerman1@hotmail.com>
2020-11-12 18:32:26 -0500

    Now gets all customers, better error handling

--- Proactive Bot/services/customers.js
+++ Proactive Bot/services/customers.js
@@ -1,31 +1,41 @@
 class CustomerService {
 
     async getLatestCustomer() {
+        const customers = await this.getCustomers();
+        if (customers && customers.length > 0) {
+            return customers[customers.length-1];
+        }
+        else {
+            return null;
+        }
+    }
+
+    async getCustomers() {
 
         let result;
 
         try {
-            const url = `${process.env.CrmUrl}:${process.env.CrmPort}/api/customers/1`;
+            const url = `${process.env.CrmUrl}:${process.env.CrmPort}/api/customers/`;
             const response = await fetch(url, {
                 method: "GET",
                 headers: { Accept: "application/json" }
-            })
+            });
     
             console.log (`Received customers with status code ${response.status}`);
     
             if (response.status !== 200) {
                 throw `Response error ${response.status}: ${response.statusText}`;
             }
-            if (response.size <= 0) {
-                throw `Empty response`;
-            }
 
             result = await response.json();
+
+            return result
     
         }
         catch (error) {
+            console.log(error);
             result = 
-            {
+            [{
                 "id": 1,
                 "firstName": `Error fetching customer data`,
                 "lastName": "",
@@ -39,7 +49,7 @@ class CustomerService {
                 "orders": [],
                 "latitude": 0,
                 "longitude": 0
-            };
+            }];
         }
         
         return result;
